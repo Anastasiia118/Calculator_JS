c6d37244ccabbf564c0218fe5143effb9ef293cf
Anastasiia <118ana@gmail.com>
2023-01-08 10:44:58 +0100

    ac, num, operators btns work

---
+++ calc.js
@@ -0,0 +1,48 @@
+let a = ''; //first number
+let b = ''; //second number
+let sign = ''; // operator sign
+let finish = false;
+
+const digit = ['0','1','2','3','4','5','6','7','8','9','.'];
+const action = ['-','+','X','/'];
+
+const out = document.querySelector('.calc-screen p');
+
+function clearAll () {
+    a = '';
+    b = '';
+    sign = '';
+    out.textContent = 0;
+}
+
+document.querySelector('.ac').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+    if(!event.target.classList.contains('btn')) return;
+    if(event.target.classList.contains('ac')) return;
+
+    out.textContent = '';
+    const key = event.target.textContent;
+
+    // if 0-9 or '.' was clicked
+    if (digit.includes(key)) {
+        if(b === '' && sign === ''){
+            a += key;
+            out.textContent = a;
+        } else if ( a!== '' && b!=='' && finish){
+
+        } else {
+            b += key;
+            out.textContent = b;
+        }
+        console.log(a,b , sign);
+        return;
+    }
+
+    if(action.includes(key)){
+        sign = key;
+        out.textContent = sign;
+        console.log(sign);
+        return;
+    }
+}
